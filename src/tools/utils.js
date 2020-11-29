ea3f78b56bca6a2b06212d53c13fee24c310611f
jianzi0307 <jianzi0307@qq.com>
2020-11-29 21:16:23 +0800

    chore

--- src/tools/utils.js
+++ src/tools/utils.js
@@ -1,8 +1,6 @@
-/* eslint-disable */
-
 /**
  * rem 相关的工具
- * @param {*}  
+ * @param {*}
  */
 export const remUtil = {
   // 调整窗口或者翻转屏幕事件
@@ -10,47 +8,47 @@ export const remUtil = {
   // 事件句柄
   handlers: [],
   // 添加设置rem的事件
-  addRemHandler: function(designWith){
-    function recalc(){
-      const docEl = document.documentElement;
-      const clientWidth = docEl.clientWidth;
-      if (!clientWidth) return;
-      docEl.style.fontSize = 100 * (clientWidth / Number(designWith))+ 'px';
+  addRemHandler: function (designWith) {
+    function recalc () {
+      const docEl = document.documentElement
+      const clientWidth = docEl.clientWidth
+      if (!clientWidth) return
+      docEl.style.fontSize = 100 * (clientWidth / Number(designWith)) + 'px'
     };
-      
-    if (!document.addEventListener) return;
-    const handler = throttle(recalc,500);
-    remUtil.handlers.push(handler);
-    window.addEventListener(remUtil.resizeEvent, handler, false);
-    document.addEventListener('DOMContentLoaded', recalc, false);       
+
+    if (!document.addEventListener) return
+    const handler = throttle(recalc, 500)
+    remUtil.handlers.push(handler)
+    window.addEventListener(remUtil.resizeEvent, handler, false)
+    document.addEventListener('DOMContentLoaded', recalc, false)
   },
   // 移除设置rem的事件
-  removeRemHandler:function(){
-    remUtil.handlers.forEach((handler)=>{
-      window.removeEventListener(remUtil.resizeEvent, handler);
+  removeRemHandler: function () {
+    remUtil.handlers.forEach((handler) => {
+      window.removeEventListener(remUtil.resizeEvent, handler)
     })
-  },
+  }
 }
 
 /**
  * 删除html上的行内style
  */
-export function removeAttributeOfHtml(){
-  const docEl = document.documentElement;
-  if(!docEl) return;
-  docEl.removeAttribute('style');
+export function removeAttributeOfHtml () {
+  const docEl = document.documentElement
+  if (!docEl) return
+  docEl.removeAttribute('style')
 }
 
 /**
  * viewport的宽度是否比某个值大
- * @param {*} width 
+ * @param {*} width
  */
-export function isWider(width){
-  const viewPortSize = getViewportSize();
-  if(viewPortSize.width>width){
-    return true;
+export function isWider (width) {
+  const viewPortSize = getViewportSize()
+  if (viewPortSize.width > width) {
+    return true
   }
-  return false;
+  return false
 }
 
 /**
@@ -58,42 +56,31 @@ export function isWider(width){
  */
 export function getViewportSize () {
   return {
-      width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
-      height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-  };
-}
-
-/**
- * 设置样式
- * @param {*} href 
- */
-export function setLink(className) {
-  let heads = document.getElementsByTagName('head');
-  let htmlTags = document.getElementsByTagName('html');
-  let htmlTag = htmlTags ? htmlTags[0] : null;
-  htmlTag.setAttribute('class',className);
+    width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+    height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+  }
 }
 
 /**
  * 节流函数
  */
-export function throttle(action,delay){
-  let timeout = null;
-  let lastRun = 0 ;
-  return function(){
-    if(timeout) return;
-    let elapsed = Date.now()- lastRun
-    let context = this;
-    let args = arguments;
-    let runCallback = function(){
-      lastRun = Date.now();
-      timeout = false;
-      action.apply(context,args);
+export function throttle (action, delay) {
+  let timeout = null
+  let lastRun = 0
+  return function () {
+    if (timeout) return
+    let elapsed = Date.now() - lastRun
+    let context = this
+    let args = arguments
+    let runCallback = function () {
+      lastRun = Date.now()
+      timeout = false
+      action.apply(context, args)
     }
-    if(elapsed>=delay){
-      runCallback();
+    if (elapsed >= delay) {
+      runCallback()
     } else {
-      timeout = setTimeout(runCallback,delay);
+      timeout = setTimeout(runCallback, delay)
     }
   }
 }
