a757008e01690ca9481ef825475b115a383fa216
jianzi0307 <jianzi0307@qq.com>
2020-11-29 21:06:22 +0800

    chore

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-import '../../common/css/com.scss';
+import '@/common/css/com.scss';
 import './index.scss';
-import {throttle} from '../../tools/utils';
+import {throttle} from '@/tools/utils';
 console.log(throttle);
 
 console.log('首页js')
